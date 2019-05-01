39963e0f5ea72073e1fa4ddcbaa8ee9caf73ff20
Zac Henney <zachenney@sfs-wifi-dhcp-10-143-219-202.wifi.sfsu.edu>
2019-04-30 20:09:20 -0700

    Search: Search functionality added to home page

--- movie_rank/routes/search.js
+++ movie_rank/routes/search.js
@@ -2,11 +2,18 @@ var express = require('express');
 var router = express.Router();
 const db = require('../db/search.js')
 
+router.post('/', (req, res, next) => {
+	const movieTitle = req.body.movieTitle
+	console.log(movieTitle)
+	res.redirect(`/search/${movieTitle}`)
+	//res.redirect('/')
+})
 
 router.get('/:movieTitle', (req, res, next)=>{
 	const movieTitle = req.params.movieTitle
 	db.getMovie(movieTitle)
 		.then(data => {
+			//res.redirect('/result', {data})
 			res.send(data)
 		})
 });
